506ec497d74a23fee94daf6ea92bf361f7bbc76b
DIvkov575 <Dmitriy@Ivkov.net>
2023-02-08 21:01:24 -0500

    tengo preguntas

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::env;
+use std::fs;
+fn cp_cont(append: bool, content: &str, file_path: &str){
+    println!("{}", &content);
+    // if (append==true){
+    //
+    // }
+}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // let content= fs::read_to_string(&args[1]).unwrap;
+    // println!("{}", &args[2])
+    // println!("{}", content);
+    // match args[2] {
+    //     
+    // }
+    let content = String::from("");
+
+    let mut iter = args.iter();
+    iter.next()
+    loop {
+        let elem = iter.next();
+        println!("{}", elem.unwrap());
+        // match iter.next() {
+        //     Some(inner) => elem,
+        //     None        => _,
+        // }
+        // if elem.eq(">") {
+        //     // cp_cont(false, content, iter.next());
+        // } else if elem.eq(">>") {
+        //     // cp_cont(true, content, iter.next());
+        // } else {
+
+        // content += fs::read_to_string(elem).unwrap();
+        /* } */
+
+    }
+}
+
